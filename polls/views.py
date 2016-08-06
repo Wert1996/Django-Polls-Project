7eb0ebee1a08e9798b665f1a15685b145044d51f
IMG <user@domain.com>
2016-08-06 23:36:00 +0530

    Tests added(5th tut done)

--- polls/views.py
+++ polls/views.py
@@ -2,19 +2,22 @@ from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
-
+from django.utils import timezone
 from polls.models import Question,Choice
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
-
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
-
+    return Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
+    
 class DetailView(generic.DetailView):
     model=Question
     template_name='polls/detail.html'
+    def get_queryset(self):
+        return Questions.objects.filter(pub_date__lte=timezone.now())
 
 '''neeche dekhiyo ek baar'''
 class ResultsView(generic.DetailView):
